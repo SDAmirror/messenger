89d0a7317ebef5a1c31e6204841bb884f8ed51ea
SDAmirror <d.sadykov@astanait.edu.kz>
2022-03-10 00:40:26 +0600

    Thu Mar 10 00:40:26     2022

--- messenger/SERVER/SERVER.py
+++ messenger/SERVER/SERVER.py
@@ -1,100 +1,95 @@
-import socket
-import json
-from threading import Thread
-
-# server's IP address
-SERVER_HOST = "localhost"
-SERVER_PORT = 5002 # port we want to use
-separator_token = "<SEP>" # we will use this to separate the client name & message
-
-# initialize list/set of all connected client's sockets
-client_sockets = {} # TODO database of clients/ session
-client_DB = {}
-
-# create a TCP socket
-s = socket.socket()
-# make the port as reusable port
-s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-# bind the socket to the address we specified
-s.bind((SERVER_HOST, SERVER_PORT))
-# listen for upcoming connections
-s.listen(5)
-print(f"[*] Listening as {SERVER_HOST}:{SERVER_PORT}")
-
-
-def listen_for_client(cs):
-    """
-    This function keep listening for a message from `cs` socket
-    Whenever a message is received, broadcast it to all other connected clients
-    """
-    while True:
-        try:
-            # keep listening for a message from `cs` socket
-            msg = cs.recv(1024).decode()
-        except Exception as e:
-            # client no longer connected
-            # remove it from the set
-            print(f"[!] Error: {e}")
-            for key, value in dict(client_sockets).items():
-                if value == cs:
-                    del client_sockets[key]
-        else:
-            # if we received a message, replace the <SEP>
-            # token with ": " for nice printing
-            msg = msg.replace(separator_token, ": ")
-        # iterate over all connected sockets'
-        messParts = msg.split('----')
-        client_sockets[messParts[1]].send(msg.encode())
-
-
+# import socket
+# import json
+# from threading import Thread
+#
+# # server's IP address
+# SERVER_HOST = "localhost"
+# SERVER_PORT = 5002 # port we want to use
+# separator_token = "<SEP>" # we will use this to separate the client name & message
+#
+# # initialize list/set of all connected client's sockets
+# client_sockets = {} # TODO database of clients/ session
+# client_DB = {}
+#
+# # create a TCP socket
+# s = socket.socket()
+# # make the port as reusable port
+# s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+# # bind the socket to the address we specified
+# s.bind((SERVER_HOST, SERVER_PORT))
+# # listen for upcoming connections
+# s.listen(5)
+# print(f"[*] Listening as {SERVER_HOST}:{SERVER_PORT}")
+#
+#
+# def listen_for_client(cs):
+#     """
+#     This function keep listening for a message from `cs` socket
+#     Whenever a message is received, broadcast it to all other connected clients
+#     """
+#     while True:
+#         try:
+#             # keep listening for a message from `cs` socket
+#             msg = cs.recv(1024).decode()
+#         except Exception as e:
+#             # client no longer connected
+#             # remove it from the set
+#             print(f"[!] Error: {e}")
+#             for key, value in dict(client_sockets).items():
+#                 if value == cs:
+#                     del client_sockets[key]
+#         else:
+#             # if we received a message, replace the <SEP>
+#             # token with ": " for nice printing
+#             msg = msg.replace(separator_token, ": ")
+#         # iterate over all connected sockets'
+#         messParts = msg.split('----')
+#         client_sockets[messParts[1]].send(msg.encode())
 
 
-while True:
-    # we keep listening for new connections all the time
-    client_socket, client_address = s.accept()
-    print(client_socket)
-
-    print(f"[+] {client_address} connected.")
-    so = client_socket.fileno()
-
-    print("{} :: :: {}".format(type(so), so))
-    ss = socket.fromfd( so,socket.AF_INET, socket.SOCK_STREAM)
-    client_socket.close()
-    print(ss)
-    # add the new connected client to connected sockets
-    try:
-        un = ss.recv(1024).decode()
-        client_sockets[un]=ss
-
-    except Exception as e:
-        # client no longer connected
-        # remove it from the set
-        print(f"[!] Error: {e}")
-
-    # start a new thread that listens for each client's messages
-    t = Thread(target=listen_for_client, args=(ss,))
-    # make the thread daemon so it ends whenever the main thread ends
-    t.daemon = True
-    # start the thread
-    t.start()
-
-
-# close client sockets
-for key in list(client_sockets.keys()):
-    client_sockets[key].close()
-# close server socket
-s.close()
+#
+#
+# while True:
+#     # we keep listening for new connections all the time
+#     client_socket, client_address = s.accept()
+#     print(client_socket)
+#
+#     print(f"[+] {client_address} connected.")
+#     so = client_socket.fileno()
+#
+#     print("{} :: :: {}".format(type(so), so))
+#     ss = socket.fromfd( so,socket.AF_INET, socket.SOCK_STREAM)
+#     client_socket.close()
+#     print(ss)
+#     # add the new connected client to connected sockets
+#     try:
+#         un = ss.recv(1024).decode()
+#         client_sockets[un]=ss
+#
+#     except Exception as e:
+#         # client no longer connected
+#         # remove it from the set
+#         print(f"[!] Error: {e}")
+#
+#     # start a new thread that listens for each client's messages
+#     t = Thread(target=listen_for_client, args=(ss,))
+#     # make the thread daemon so it ends whenever the main thread ends
+#     t.daemon = True
+#     # start the thread
+#     t.start()
+#
+#
+# # close client sockets
+# for key in list(client_sockets.keys()):
+#     client_sockets[key].close()
+# # close server socket
+# s.close()
 
 
 #-------------------------------------------------------------------------------------------------------------------
-
-
-def fib(n):
-    if n <= 2:
-        return 1
-    else:
-        return fib(n-1)+fib(n-2)
-
+import socket
+import uuid
+import json
 
 
 from socket import *
@@ -103,7 +98,7 @@ from concurrent.futures import ProcessPoolExecutor as Pool
 from collections import deque
 from select import select
 
-
+clients = {}
 tasks = deque()
 pool = Pool(10)
 recv_wait = {}
@@ -117,8 +112,17 @@ def future_done(future):
 def future_monitor():
     while True:
         yield 'recv',future_event
-        future_event.recv(100)
+        future_event.recv(1024)
 tasks.append(future_monitor())
+class somestruct:
+    def __init__(self,client,id):
+        self.client = client
+        self.id = id
+
+def message_processor(n):
+    return 1
+
+
 def run():
 
     while any([tasks,recv_wait,send_wait]):
@@ -144,16 +148,18 @@ def run():
         except StopIteration:
             print('task done')
 
-def fib_handler(client):
+def client_handler(client):
     while True:
         yield'recv',client
 
         #TODO try catch if client disconnected then delete this socket
-        req = client.recv(100)
-        if not req:
+        #message handle
+        message = client.recv(1024).decode()
+        if not message:
             break
-        n = int(req)
-        future = pool.submit(fib,n)
+
+
+        future = pool.submit(message_processor, message)
         yield 'future',future
         result = future.result()
         # result = fib(n)
@@ -162,11 +168,11 @@ def fib_handler(client):
         try:
             client.send(resp)
         except:
-            print('clint disconnectrd')
+            print('cleint disconnected')
     print('closed')
 
 
-def fib_server(address):
+def base_server(address):
     sock = socket(AF_INET, SOCK_STREAM)
     sock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
     sock.bind(address)
@@ -175,20 +181,34 @@ def fib_server(address):
     while True:
         yield 'recv',sock
         client, addr = sock.accept()
+        message = client.recv(1024).decode()
+
+        data = sock.recv(1024).decode('utf-8')  # this returns a JSON-formatted String
+        #TODO function that starts cliebnt at connection.
+        parsed_data = json.loads(data)
+        id = uuid.UUID
         print(f"[+] {client} connected.")
+        clients[id] =client
         # t = Thread(target=fib_handler,args=(client,),daemon=True)
         # t.start()
         # fib_handler(client)
-        tasks.append(fib_handler(client))
+        tasks.append(somestruct(client_handler(client),id))
 
 
 
 
 
-tasks.append(fib_server(('localhost',8888)))
+tasks.append(base_server(('localhost', 8888)))
 
 if __name__ == '__main__':
     run()
 
 
 
+#postgresql cursor
+
+# 20000-50000
+
+#while online
+    # cursor work
+#
